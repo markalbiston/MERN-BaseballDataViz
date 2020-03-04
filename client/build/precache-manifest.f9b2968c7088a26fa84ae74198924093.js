eaa398f9e0162dc954f23b743a919740071bfd58
Mark Albiston <mark.albiston@gmail.com>
2020-03-04 08:54:48 -0700

    actually with build folder

---
+++ client/build/precache-manifest.f9b2968c7088a26fa84ae74198924093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba24bd445d6e8140ce344c7df57585db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17875153b124094f2b33",
+    "url": "/static/css/main.683075b8.chunk.css"
+  },
+  {
+    "revision": "e7de3d0778f94232a0ef",
+    "url": "/static/js/2.1e32424e.chunk.js"
+  },
+  {
+    "revision": "6dbd9e7d4b65b27382d6cb0964c77a31",
+    "url": "/static/js/2.1e32424e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17875153b124094f2b33",
+    "url": "/static/js/main.cce1a095.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "71f1b5c9aa29e2948c083c602e49b943",
+    "url": "/static/media/baseball04.71f1b5c9.png"
+  }
+]);
